9be46f8daa72b20f104bb10cfac883b2d5395884
Naoki <naoki.joy@gmail.com>
2026-01-28 16:51:41 +0900

    Fix: Switch to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Also ignore TS errors during build to ensure deployment
     ignoreBuildErrors: true,
   },
 };
